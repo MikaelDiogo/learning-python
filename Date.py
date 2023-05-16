4df0a7a0c370219931e764abacc5a213eaee3018
Mikael Diogo <102759939+MikaelDiogo@users.noreply.github.com>
2023-05-15 22:22:16 -0300

    Add files via upload

---
+++ Date.py
@@ -0,0 +1,7 @@
+from datetime import datetime, timedelta
+
+date_now = datetime.now()
+date_future = date_now + timedelta(2)
+print(date_future)
+
+#This code shows what the date would be two days in the future
